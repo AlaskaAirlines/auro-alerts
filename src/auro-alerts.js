1d8f6cc1ee4689c6af898812290090cfaed2534f
James Jackson <james.jackson2@alaskaair.com>
2020-08-06 13:42:29 -0700

    refactor: update template with initial start of component

--- src/auro-alerts.js
+++ src/auro-alerts.js
@@ -5,20 +5,45 @@
 
 import { LitElement, html, css } from "lit-element";
 
+// Import Icons
+import error from '@alaskaairux/icons/dist/icons/alert/error_es6.js';
+import warning from '@alaskaairux/icons/dist/icons/alert/warning-stroke_es6.js';
+import information from '@alaskaairux/icons/dist/icons/alert/information-stroke_es6.js';
+import question from '@alaskaairux/icons/dist/icons/alert/question-stroke_es6.js';
+
 // Import touch detection lib
 import "focus-visible/dist/focus-visible.min.js";
 import styleCss from "./style-css.js";
 
 // build the component class
 class AuroAlerts extends LitElement {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+    this.errorDOM = new DOMParser().parseFromString(error.svg, 'text/html');
+    this.errorSVG = this.errorDOM.body.firstChild;
+    this.warningDOM = new DOMParser().parseFromString(warning.svg, 'text/html');
+    this.warningSVG = this.warningDOM.body.firstChild;
+    this.informationDOM = new DOMParser().parseFromString(information.svg, 'text/html');
+    this.informationSVG = this.informationDOM.body.firstChild;
+    this.questionDOM = new DOMParser().parseFromString(question.svg, 'text/html');
+    this.questionSVG = this.questionDOM.body.firstChild;
+  }
 
   // function to define props used within the scope of thie component
   static get properties() {
     return {
-      cssClass:   { type: String }
+      error: {
+        type: Boolean,
+        reflect: true
+      },
+      warning: {
+        type: Boolean,
+        reflect: true
+      },
+      information: {
+        type: Boolean,
+        reflect: true
+      }
     };
   }
 
@@ -31,8 +56,13 @@ class AuroAlerts extends LitElement {
   // function that renders the HTML and CSS into  the scope of the component
   render() {
     return html`
-      <div class=${this.cssClass}>
-        <slot></slot>
+      <div class="alert">
+        ${this.error ? html`<div class="icon">${this.errorSVG}</div>` : html``}
+        ${this.warning ? html`<div class="icon">${this.warningSVG}</div>` : html``}
+        ${this.information ? html`<div class="icon">${this.informationSVG}</div>` : html``}
+        <div class="content">
+          <slot></slot>
+        </div>
       </div>
     `;
   }
